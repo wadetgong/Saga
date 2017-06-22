ccc01609db609405977f0cbfced5e8351191dda1
ehacinom <ehacinom@gmail.com>
2017-06-22 10:45:46 -0500

    story screen recieving data

--- App/Containers/StoryScreen.js
+++ App/Containers/StoryScreen.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { View, FlatList } from 'react-native'
-import { getStories } from '../Services/FixtureApi' 
+import { View, ListItem, Text } from 'react-native'
+import API from '../Services/FixtureApi' 
 
 // import styles from './Styles/StoryScreen' // not there
 export default class StoryScreen extends React.Component {
     render () {
-        console.log(getStories().data)
-        
+        const data = API.getStories().data;
+        console.log('data in SToryScreen', data)
         return (
-            <FlatList 
-                data={getStories().data}
-                renderItem={({item}) => <ListItem title={item.title} />}
-            />
+            <View>
+            </View>
         )
     }
 }
