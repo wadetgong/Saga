e806df20ba55a16f4aba94f42654f4c27a284aaf
ehacinom <ehacinom@gmail.com>
2017-06-23 15:17:53 -0500

    firebase connected

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,20 @@
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
+import * as firebase from 'firebase'
+import secrets from './secrets'
 
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: secrets.ios_apiKey,
+  authDomain: secrets.ios_authDomain,
+  databaseURL: secrets.ios_databaseURL,
+  storageBucket: secrets.ios_storageBucket,
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+console.log('index.ios.js firebaseApp\n', firebaseApp)
+
+// register app
 AppRegistry.registerComponent('TestIgniteApp', () => App)
