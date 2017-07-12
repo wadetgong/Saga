005004431091ddd7774d00ce047d39c65a85fa40
ehacinom <ehacinom@gmail.com>
2017-07-12 10:35:59 -0500

    Redux WIP geolocation

---
+++ App/Redux/GeolocationRedux.js
@@ -0,0 +1,38 @@
+// actions
+const UPDATE_LOCATION = 'UPDATE_LOCATION'
+
+// action-creator
+const setLocation = location => ({
+  type: UPDATE_LOCATION,
+  location
+})
+
+// reducer
+const initialState = {
+  location: {
+    latitude: 41.885731,
+    longitude: -87.633957,
+  }
+}
+
+export default const reducer = (state = initialState, action) => {
+  console.log('Reducer of Geolocation')
+  const newState = Object.assign({}, state)
+  switch (action.type) {
+    case UPDATE_LOCATION:
+      // newState.location = action.location
+      newState.location = {
+        latitude: 41.885731,
+        longitude: -87.633957,
+      }
+      break
+    default:
+      return state
+  }
+  return newState
+}
+
+// action-dispatcher
+export const updateLocation = location => dispatch => {
+  dispatch(setLocation(location))
+}
--- App/Redux/constants.js
+++
@@ -1,8 +0,0 @@
-// change these thresholds higher to make more stable but not too high to where we can never rotate towards it
-//maybe move_factor can help? im not sure have not edited it
-
-export const GYRO_MOVE_THRESHOLD_X = .10; //.055
-export const GYRO_MOVE_THRESHOLD_Y = .10;
-
-export const MOVE_FACTOR_Y = 40;
-export const MOVE_FACTOR_X = MOVE_FACTOR_Y * .8;
