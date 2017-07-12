5c3e70ccc9b2632f0ee2467756d2cc596883dbef
ehacinom <ehacinom@gmail.com>
2017-07-12 13:08:08 -0500

    fix Geolocation

--- App/Redux/GeolocationRedux.js
+++ App/Redux/GeolocationRedux.js
@@ -16,15 +16,10 @@ const initialState = {
 }
 
 export const reducer = (state = initialState, action) => {
-  console.log('Reducer of Geolocation')
   const newState = Object.assign({}, state)
   switch (action.type) {
     case UPDATE_LOCATION:
-      // newState.location = action.location
-      newState.location = {
-        latitude: 41.885731,
-        longitude: -87.633957,
-      }
+      newState.location = action.location
       break
     default:
       return state
